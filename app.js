4864385f2f7b7b99164edd35f4af418fbb2528d6
crissand16 <cristian.a.gomez16@gmail.com>
2026-08-31 10:00:51 -0500

    Envio de informacion formato JSON

--- app.js
+++ app.js
@@ -4,12 +4,41 @@ const express = require('express');
 const app = express();
 const PORT = process.env.MIPUERTO || 3003;
 
-
+//middelware body-parse
+app.use(express.json())
 app.get('/', (req, res) => {
-  res.send('API Rest Full con express');
+  res.send('API Rest Full con Express');
 });
 
 
+app.get('/api/aprendices', (req, res) => {
+  res.status(200).json({'Mensaje': 'Lista Aprendices'})
+})
+
+
+app.post('/api/aprendices', (req, res) => {
+  const edad = req.body.edad
+  const datosAprendiz = req.body
+
+  if (edad >= 18) {
+    return res.status(201).json({'Mensaje': 'Eres mayor de Edad','Datos': datosAprendiz})}
+
+    res.status(201).json({'Mensaje': 'Eres menor de Edad', 'Datos': datosAprendiz})
+//  res.status(201).json({'Mensaje': 'Crear Aprendiz', 'Datos': datosAprendiz,})
+})
+
+
+app.put('/api/aprendices/:id', (req, res) => {
+  res.status(200).json({'Mensaje': 'Actualiza Aprendiz'})
+})
+
+
+app.delete('/api/aprendices/:id', (req, res) => {
+  res.status(200).json({'Mensaje': 'Eliminado'})
+})
+
+
+
 app.listen(PORT, () => {
-  console.log(`Servidor online en el puerto ${PORT}`);
+  console.log(`Servidor online en el puerto: http://localhost:${PORT}`);
 });
